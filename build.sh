99e282e1d59df7e9f8f43ea7a49d2266edc87903
Ubuntu <ubuntu@ip-172-31-40-131.ap-south-1.compute.internal>
2023-07-19 16:33:09 +0000

    done

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#set the image name
+ IMAGENAME= "myreactapp"
+
+#build the image
+ docker build -t $IMAGENAME .
+
+ #tag the image
+ docker tag $IMAGENAME jeevithals25/$IMAGENAME
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+
+#set the image
+IMAGENAME="myreactapp"
+
+ #set the branch name
+branch=$(https://github.com/jeevitha700/capstoneproject.git)
+
+#if the branch equal to dev, build the image and push it to dockerhub dev repository
+if [ "$branch"  == "dev" ];
+then
+    
+     docker build -t $IMAGENAME .
+     docker push jeevithals25/dev:$IMAGENAME
+ else
+    [ "$branch" == "prod" ]; 
+then  
+    docker build -t $IMAGENAME .
+    docker push jeevithals25/prod:$IMAGENAME
+ fi
