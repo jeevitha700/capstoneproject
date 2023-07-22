d13f2a97808f9529bc7ee9892ee94393d90d60a3
Ubuntu <ubuntu@ip-172-31-32-60.ap-south-1.compute.internal>
2023-07-22 12:35:34 +0000

    done

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,22 @@
 
 #set the image
 IMAGENAME="myreactapp"
-
+USERNAME=jeevithals25!
+PASSWORD=girijadvg
  #set the branch name
- branch=$"https://github.com/jeevitha700/capstoneproject.git"
+ branch='https://github.com/jeevitha700/capstoneproject.git'
 
 #if the branch equal to dev, build the image and push it to dockerhub dev repository
 if [ "$branch"  == "dev" ];
 then
     
      docker build -t $IMAGENAME .
+     docker login --username $USERNAME --password $PASSWORD 
      docker push jeevithals25/dev:$IMAGENAME
  elif
     [ "$branch" == "prod" ];   
 then
     docker build -t $IMAGENAME .
+    docker login --username $USERNAME --password $PASSWORD
     docker push jeevithals25/prod:$IMAGENAME
  fi
