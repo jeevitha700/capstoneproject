23a5d2de04172b5f1aecd6f1193b305937381e61
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-24 13:48:00 +0530

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set the image
 IMAGENAME="myreactapp"
-branch="$https://github.com/jeevitha700/capstoneproject.git"
+branch=" $https://github.com/jeevitha700/capstoneproject.git "
 if [ "$branch"= "dev" ] ;then
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
