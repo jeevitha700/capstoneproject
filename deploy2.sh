a6bcc0cd404e9f93f7ef777060071032b4a19c88
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-25 12:19:19 +0530

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set the image
 IMAGENAME="myreactapp"
-branch=returnStdout: true, script: git rev-parse --abbrev-ref HEAD
+branch= returnStdout: true , script: git rev-parse --abbrev-ref HEAD
 if [ '$branch' == 'dev' ] ; then
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
