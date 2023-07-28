6361c4696be9ec0e4bc4f13989178785a5bd3f61
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-28 12:54:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,15 @@ IMAGENAME="myreactapp"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if [ "$BRANCH" == "dev" ]; then
+    echo "Building image for dev branch
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
     docker push jeevithals25/dev:latest
     docker logout
 
-elif [ "$BRANCH" == "prod" ]; then 
+else
+
+   [ "$BRANCH" == "prod" ]; then 
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
     docker push jeevithals25/prod:latest
