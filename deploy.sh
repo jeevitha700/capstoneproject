bebccd70dc32dfd7b86ce32bef5b1bcde7ca006b
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-28 13:46:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ if [ "$BRANCH" == "dev" ]; then
     docker logout
 
 elif [ "$BRANCH" == "prod" ]; then
-    echo "Building image for prod branch ..."
-    docker build -t $IMAGENAME .
-    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
-    docker push jeevithals25/prod:latest
-    docker logout
+      echo "Building image for prod branch ..."
+      docker build -t $IMAGENAME .
+      echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
+      docker push jeevithals25/prod:latest
+      docker logout
  else
 
     echo "exit the code is 1"
