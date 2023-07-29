9dbd9f49bf7a504e4e8a9976c36823b2c74a3745
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-29 21:25:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,19 @@
 #set the image
 IMAGENAME="myreactapp"
 
-branch=$ git branch *
-
-if [ "$GIT_BRANCH" == "dev" ]; then
+if [ "$BRANCH" == "dev" ]; then
     echo "Building image for dev branch "
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
     docker push jeevithals25/dev:latest
     docker logout
 
-elif [ "$GIT_BRANCH" == "prod" ]; then
+elif [ "$BRANCH" == "prod" ]; then
       echo "Building image for prod branch ..."
       docker build -t $IMAGENAME .
       echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
       docker push jeevithals25/prod:latest
       docker logout
  else 
-       echo"$branch" IS INVALID
+       echo"$BRANCH" IS INVALID
 fi
