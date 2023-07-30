b6f6a45d370720f177f0d6cecac3f4c5464748b7
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-30 08:14:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #set the image
 IMAGENAME="myreactapp"
-branch=$GIT_BRANCH.substringAfterLast{'/'}
-if [ "$BRANCH" == "dev" ]; then
+branch=${GIT_BRANCH.substringAfterLast['/'] }
+if[ "$BRANCH" == "dev" ]; then
     echo "Building image for dev branch "
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
