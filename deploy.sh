fbc9023406d8254809c917e79dd01c2ee70fe13c
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-08-02 08:37:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ IMAGENAME="myreactapp"
  #If  the branch is dev, build the image and push it to docker-hub dev repository
 if [ "$BRANCH" == "dev" ]; then
     echo "Building image for dev branch "
-    docker build -t $IMAGENAME.
+    docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
     docker tag myreactapp jeevithals25/dev
     docker push jeevithals25/dev:latest
@@ -13,7 +13,7 @@ if [ "$BRANCH" == "dev" ]; then
 
 elif [ "$BRANCH" == "prod" ]; then
       echo "Building image for prod branch ..."
-      docker build -t $IMAGENAME/prod:latest .
+      docker build -t $IMAGENAME .
       echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
       docker tag myreactapp jeevithals25/prod
       docker push jeevithals25/prod:latest
