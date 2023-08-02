b9e3421b3c678477863debef099c08903312fa0f
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-08-02 08:36:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,22 @@
 #!/bin/bash
 #set the image
 IMAGENAME="myreactapp"
-export USERNAME="jeevithals25"
-export PASSWORD="girijadvg"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
- if [ "$BRANCH" == "dev" ] ;
- then
+  #If  the branch is dev, build the image and push it to docker-hub dev repository
+if [ "$BRANCH" == "dev" ]; then
+    echo "Building image for dev branch "
     docker build -t $IMAGENAME .
-    docker login --username $USERNAME --password $PASSWORD
-     docker tag myreactapp jeevithals25/dev
+    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
+    docker tag myreactapp jeevithals25/dev
     docker push jeevithals25/dev:latest
+    docker logout
 
-elif [ "$BRANCH" == "prod" ] ;
-then
-    docker build -t $IMAGENAME .
-    docker login --username $USERNAME --password $PASSWORD
-    docker tag myreactapp jeevithals25/prod
-    docker push jeevithals25/prod:latest
+elif [ "$BRANCH" == "prod" ]; then
+      echo "Building image for prod branch ..."
+      docker build -t $IMAGENAME .
+      echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
+      docker tag myreactapp jeevithals25/prod
+      docker push jeevithals25/prod:latest
+      docker logout
 
- fi
+fi
